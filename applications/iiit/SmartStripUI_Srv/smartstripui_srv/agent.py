0b3604fb758b97530c29cbd95931e4e42bee69f7
sambabug <sambabug@gmail.com>
2016-08-18 22:47:46 +0530

    ...

--- applications/iiit/SmartStripUI_Srv/smartstripui_srv/agent.py
+++ applications/iiit/SmartStripUI_Srv/smartstripui_srv/agent.py
@@ -46,134 +46,140 @@ def DatetimeFromValue(ts):
 def smartstripui_srv(config_path, **kwargs):
 
     config = utils.load_config(config_path)
-    agent_id = config['agentid']
-    server_address = config.get('server_address', '127.0.0.1')
-    server_port = int(config.get('server_port', 7575))
-    backlog = int(config.get('backlog', 10))
     
-    class SmartStripUI_Srv(PublishMixin, BaseAgent):
-        '''
-        Volttron Server - listens for connections from UI Layer/Apps 
-        and publishes the request to Volttron Bus
-        '''
-        def __init__(self, config_path, **kwargs):
-            _log.debug('__init__()')
-            super(SmartStripUI_Srv, self).__init__(**kwargs)
-            
-            self.ssui_socket = None
-            self.state = None
-            
-        def setup(self):
-            _log.debug('setup()')
-            _log.info(config['message'])
-            self._agent_id = config['agentid']
-            
-            '''Perform additional setup.'''
-            super(SmartStripUI_Srv, self).setup()
-
-            # Open a socket to listen for incoming connections
-            self.ssui_socket = sock = socket.socket()
-            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            sock.bind(tuple([server_address, server_port]))
-            sock.listen(backlog)
-
-            # Register a callback to accept new connections
-            self.reactor.register(self.ssui_socket, self.handle_accept)
-            
-        def handle_accept(self, ssui_sock):
-            _log.debug('handle_accept()')
-            '''Accept new connections.'''
-            sock, addr = ssui_sock.accept()
-            file = sock.makefile('r+', 0)
-            _log.info('Connection {} accepted from {}:{}'.format(file.fileno(),
-                *addr))
-            try:
-                file.write('hi from UI srv')
-            except socket.error:
-                _log.info('Connection {} disconnected'.format(file.fileno()))
-            # Register a callback to recieve input from the client.
-            self.reactor.register(file, self.handle_input)
-
-        def handle_input(self, file):
-            _log.debug('handle_input()')
-            '''Recieve the data from the ble ui server'''
-            try:
-                response = file.readline()
-                if not response:
-                    raise socket.error('disconnected')
-                self.processResponse(response)
-            except socket.error:
-                _log.info('Connection {} disconnected'.format(file.fileno()))
-                self.reactor.unregister(file)
-
-        def processResponse(self, response):
-            _log.debug('processResponse()')
-            #response = response.strip()
-            #print(response)
-            try:
-                
-                rpcdata = jsonrpc.JsonRpcData.parse(response)
-                _log.info('rpc method: {}'.format(rpcdata.method))
-                
-                if rpcdata.method == "rpc_changeThresholdPP":
-                    args = {'plugID': rpcdata.params['plugID'],
-                            'newThreshold': rpcdata.params['newThreshold']}
-                    result = self.rpc_changeThresholdPP(**args)
-                elif rpcdata.method == "rpc_changeCurrentPP":
-                    _log.info('newPricePoint: {}'.format(rpcdata.params['newPricePoint']))
-                    args = {'newPricePoint': rpcdata.params['newPricePoint']}
-                    result = self.rpc_changeCurrentPP(**args)
-                    
-                return jsonrpc.json_result(rpcdata.id, result)
-                    
-            except AssertionError:
-                print('AssertionError')
-                return jsonrpc.json_error(
-                    'NA', INVALID_REQUEST, 'Invalid rpc data {}'.format(data))
-            except Exception as e:
-                print(e)
-                return jsonrpc.json_error(
-                    'NA', UNHANDLED_EXCEPTION, e
-                )
- 
-            #rpcdict = data.json()
-            #print('RPCDICT', rpcdict)
-            #if response:
-                #parse response
-                #if cmd==postData
-                    #post to volttron bus - topic UI/Requests
-                    #file.write('OK')
-                    
-        def rpc_changeThresholdPP(self, plugID, newThreshold):
-            try:
-                result = self.vip.rpc.call('iiit.smartstrip', 'setThresholdPP',
-                                            plugID, newThreshold).get(timeout=1)
-            except Exception as e:
-                _log.error ("Could not contact iiit.smartstrip. Is it running?")
-                print(e)
-                return 'FAILED'
-            return 'SUCCESS'
-
-        def rpc_changeCurrentPP(self, newPricePoint):
-            try:
-                result = self.vip.rpc.call('iiit.pricepoint', 'price_from_net', newPricePoint).get(timeout=1)
-            except Exception as e:
-                _log.error ("Could not contact iiit.pricepoint. Is it running?")
-                print(e)
-                return 'FAILED'
-            return 'SUCCESS'
-        
+    vip_identity = config.get('vip_identity', 'iiit.ssui_srv')
+    # This agent needs to be named platform.actuator. Pop the uuid id off the kwargs
+    kwargs.pop('identity', None)
 
     Agent.__name__ = 'SmartStripUI_Srv_Agent'
-    return SmartStripUI_Srv(config_path, **kwargs)
+    return SmartStripUI_Srv(config_path, identity=vip_identity, **kwargs)
+    
+class SmartStripUI_Srv(PublishMixin, Agent):
+    '''
+    Volttron Server - listens for connections from UI Layer/Apps 
+    and publishes the request to Volttron Bus
+    '''
+    def __init__(self, config_path, **kwargs):
+        _log.debug('__init__()')
+        super(SmartStripUI_Srv, self).__init__(**kwargs)
+        _log.debug("vip_identity: " + self.core.identity)
+        
+        self.config = utils.load_config(config_path)
+        self.agent_id = self.config['agentid']
+        self.server_address = self.config.get('server_address', '127.0.0.1')
+        self.server_port = int(self.config.get('server_port', 7575))
+        self.backlog = int(self.config.get('backlog', 10))
+
+        self.ssui_socket = None
+        self.state = None
+        
+    def setup(self):
+        _log.debug('setup()')
+        _log.info(self.config['message'])
+        self._agent_id = self.config['agentid']
+        
+        '''Perform additional setup.'''
+        super(SmartStripUI_Srv, self).setup()
+
+        # Open a socket to listen for incoming connections
+        self.ssui_socket = sock = socket.socket()
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(tuple([self.server_address, self.server_port]))
+        sock.listen(self.backlog)
+
+        # Register a callback to accept new connections
+        self.reactor.register(self.ssui_socket, self.handle_accept)
+        
+    def handle_accept(self, ssui_sock):
+        _log.debug('handle_accept()')
+        '''Accept new connections.'''
+        sock, addr = ssui_sock.accept()
+        file = sock.makefile('r+', 0)
+        _log.info('Connection {} accepted from {}:{}'.format(file.fileno(),
+            *addr))
+        try:
+            file.write('hi from UI srv')
+        except socket.error:
+            _log.info('Connection {} disconnected'.format(file.fileno()))
+        # Register a callback to recieve input from the client.
+        self.reactor.register(file, self.handle_input)
+
+    def handle_input(self, file):
+        _log.debug('handle_input()')
+        '''Recieve the data from the ble ui server'''
+        try:
+            response = file.readline()
+            if not response:
+                raise socket.error('disconnected')
+            self.processResponse(response)
+        except socket.error:
+            _log.info('Connection {} disconnected'.format(file.fileno()))
+            self.reactor.unregister(file)
+
+    def processResponse(self, response):
+        _log.debug('processResponse()')
+        #response = response.strip()
+        #print(response)
+        try:
+            
+            rpcdata = jsonrpc.JsonRpcData.parse(response)
+            _log.info('rpc method: {}'.format(rpcdata.method))
+            
+            if rpcdata.method == "rpc_changeThresholdPP":
+                args = {'plugID': rpcdata.params['plugID'],
+                        'newThreshold': rpcdata.params['newThreshold']}
+                result = self.rpc_changeThresholdPP(**args)
+            elif rpcdata.method == "rpc_changeCurrentPP":
+                _log.info('newPricePoint: {}'.format(rpcdata.params['newPricePoint']))
+                args = {'newPricePoint': rpcdata.params['newPricePoint']}
+                result = self.rpc_changeCurrentPP(**args)
+                
+            return jsonrpc.json_result(rpcdata.id, result)
+                
+        except AssertionError:
+            print('AssertionError')
+            return jsonrpc.json_error(
+                'NA', INVALID_REQUEST, 'Invalid rpc data {}'.format(data))
+        except Exception as e:
+            print(e)
+            return jsonrpc.json_error(
+                'NA', UNHANDLED_EXCEPTION, e
+            )
+
+        #rpcdict = data.json()
+        #print('RPCDICT', rpcdict)
+        #if response:
+            #parse response
+            #if cmd==postData
+                #post to volttron bus - topic UI/Requests
+                #file.write('OK')
+                
+    def rpc_changeThresholdPP(self, plugID, newThreshold):
+        try:
+            result = self.vip.rpc.call('iiit.smartstrip', 'setThresholdPP',
+                                        plugID, newThreshold).get(timeout=1)
+        except Exception as e:
+            _log.error ("Could not contact iiit.smartstrip. Is it running?")
+            print(e)
+            return 'FAILED'
+        return 'SUCCESS'
+
+    def rpc_changeCurrentPP(self, newPricePoint):
+        try:
+            result = self.vip.rpc.call('iiit.pricepoint', 'price_from_net', newPricePoint).get(timeout=1)
+        except Exception as e:
+            _log.error ("Could not contact iiit.pricepoint. Is it running?")
+            print(e)
+            return 'FAILED'
+        return 'SUCCESS'
+        
+
 
 
 def main(argv=sys.argv):
     '''Main method called by the eggsecutable.'''
     try:
-        utils.default_main(smartstripui_srv,
-            description='VOLTTRON platform™ server agent for to receive UI request.',
-            argv=argv)
+        utils.vip_main(smartstripui_srv)
     except Exception as e:
         print e
         _log.exception('unhandled exception')
