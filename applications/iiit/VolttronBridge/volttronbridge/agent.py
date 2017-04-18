2d06dd0f524d7292432efcfc8f787617d09d698d
sambabug <sambabug@gmail.com>
2017-04-18 14:26:12 +0530

    ...

--- applications/iiit/VolttronBridge/volttronbridge/agent.py
+++ applications/iiit/VolttronBridge/volttronbridge/agent.py
@@ -21,21 +21,20 @@ from volttron.platform.agent import utils
 from volttron.platform.messaging import headers as headers_mod
 
 from volttron.platform.messaging import topics, headers as headers_mod
-
+from volttron.platform.agent.known_identities import (
+    MASTER_WEB, VOLTTRON_CENTRAL, VOLTTRON_CENTRAL_PLATFORM)
+from volttron.platform import jsonrpc
+from volttron.platform.jsonrpc import (
+        INVALID_REQUEST, METHOD_NOT_FOUND,
+        UNHANDLED_EXCEPTION, UNAUTHORIZED,
+        UNABLE_TO_REGISTER_INSTANCE, DISCOVERY_ERROR,
+        UNABLE_TO_UNREGISTER_INSTANCE, UNAVAILABLE_PLATFORM, INVALID_PARAMS,
+        UNAVAILABLE_AGENT)
+        
 import time
 
 import requests
 import json
-
-SH_DEVICE_LED_DEBUG = 0
-SH_DEVICE_LED = 1
-SH_DEVICE_FAN = 2
-SH_DEVICE_FAN_SWING = 3
-SH_DEVICE_S_LUX = 4
-SH_DEVICE_S_RH = 5
-SH_DEVICE_S_TEMP = 6
-SH_DEVICE_S_CO2 = 7
-SH_DEVICE_S_PIR = 8
 
 utils.setup_logging()
 _log = logging.getLogger(__name__)
@@ -58,162 +57,182 @@ def volttronbridge(config_path, **kwargs):
     config = utils.load_config(config_path)
     agent_id = config['agentid']
     
-    ble_ui_server_address = config.get('ble_ui_server_address', '127.0.0.1')
-    ble_ui_server_port = int(config.get('ble_ui_server_port', 8082))
-    
-    topic_price_point = config.get('topic_price_point', \
-                                    'prices/PricePoint')
-    topic_sensorsLevelAll_point = config.get('sensorsLevelAll_point', \
-                                            'smarthub/sensors/all')
-    topic_ledState_point = config.get('ledState_point', \
-                                            'smarthub/ledstate')    
-    topic_fanState_point = config.get('fanState_point', \
-                                            'smarthub/fanstate')    
-    topic_ledLevel_point = config.get('ledLevel_point', \
-                                            'smarthub/ledlevel')    
-    topic_fanLevel_point = config.get('fanLevel_point', \
-                                            'smarthub/fanlevel')
-    topic_ledThPP_point = config.get('ledThPP_point', \
-                                            'smarthub/ledthpp')    
-    topic_fanThPP_point = config.get('fanThPP_point', \
-                                            'smarthub/fanthpp')
+    energyDemand_topic = self.config.get('energyDemand_topic', \
+                                            'zone/energydemand')  
+    pricePoint_topic = self.config.get('pricePoint_topic', \
+                                            'zone/pricepoint')
+    '''
+    the assumption is that for UpStream (us) the bridge communicates with only one instance and 
+    for the DownStream (ds) the bridge would be posting to multiple devices
 
+    for pricepoint one-to-many communication
+        energydemand one-to-one communication
+    
+    The ds devices on their start up would register with this instance with ip address & port
+    
+    The bridge is aware of the upstream devices and registers to it (associates to it). 
+    Also, as and when the is a change in energydemand, the same is posted to the upstream bridges.
+    whereas the the bridge does not upfront know the downstream devices. 
+    As and when the downstram bridges register to the bridge, the bridge starts posting the messages (pricepoint) to them
+    '''
+        
     class VolttronBridge(Agent):
         '''
-        retrive the data from volttron and pushes it to the BLE UI Server
+        retrive the data from volttron bus and pushes it to upstream or downstream volttron instance
+        if posting to downstream, then the data is pricepoint
+        and if posting to upstream then the data is energydemand
         '''
 
         def __init__(self, **kwargs):
             _log.debug('__init__()')
             super(VolttronBridge, self).__init__(**kwargs)
-            
+                        
         @Core.receiver('onsetup')
         def setup(self, sender, **kwargs):
             _log.debug('setup()')
             _log.info(config['message'])
             self._agent_id = config['agentid']
-            ble_ui_srv_address = config.get('ble_ui_server_address', '127.0.0.1')
-            ble_ui_srv_port = config.get('ble_ui_server_port', 8081)
-            self.url_root = 'http://' + ble_ui_srv_address + ':' + str(ble_ui_srv_port) + '/SmartHub'
+            
+            self._usConnected = False
+            self._bridge_host = config.get('bridge_host', 'ZONE')
+            self._deviceId    = config.get('deviceId', 'Zone-1')
+            
+            if self._bridge_host == 'ZONE':
+                
+                self._this_ip_addr    = config.get('zone_ip_addr', "192.168.1.250")
+                self._this_port       = config.get('zone_port', 8082)
+                
+                #downstream volttron instances (SmartHubs)
+                #post price point to these instances
+                self._ds_voltBr = []
+                self._ds_deviceId = []
+                
+            elif self._bridge_host == 'HUB'
+                
+                #upstream volttron instance (Zone)
+                self._up_ip_addr      = config.get('zone_ip_addr', "192.168.1.250")
+                self._up_port         = config.get('zone_port', 8082)
+                
+                self._this_ip_addr    = config.get('sh_ip_addr', "192.168.1.61")
+                self._this_port       = config.get('sh_port', 8082)
 
+                #downstream volttron instances (SmartStrips)
+                #post price point to these instances
+                self._ds_voltBr = []
+                self._ds_deviceId = []
+                
+            elif self._bridge_host == 'STRIP'
+                
+                #upstream volttron instance (Smart Hub)
+                self._up_ip_addr      = config.get('sh_ip_addr', "192.168.1.61")
+                self._up_port         = config.get('sh_port', 8082)
+                
+                self._this_ip_addr      = config.get('ss_ip_addr', "192.168.1.71")
+                self._this_port         = config.get('ss_port', 8082)
+                
+            self._discovery_address = self._this_ip_addr + ':' + self._this_port
+            
         @Core.receiver('onstart')            
         def startup(self, sender, **kwargs):
             _log.debug('startup()')
-            return
+            self.vip.rpc.call(MASTER_WEB, 'register_agent_route', \
+                    r'^/VolttronBridge', \
+                    self.core.identity, \
+                    "rpc_from_net").get(timeout=30)
 
+            if self._bridge_host == 'ZONE':
+                #downstream volttron instances (SmartHubs)
+                #do nothing
+                
+            elif self._bridge_host == 'HUB'
+                #upstream volttron instance (Zone) - register with zone VolttronBridge
+                url_root = 'http://' + self._up_ip_addr + ':' + self._up_port + '/VolttronBridge'
+                result = self.do_rpc(url_root, 'rpc_registerDsBridge', 
+                                    {'discovery_address': self._discovery_address, 
+                                        'deviceId': self._deviceId
+                                    })
+                if result == 'SUCCESS':
+                    self._usConnected = True
+                    
+                #downstream volttron instances (SmartStrips)
+                # do nothing
+                
+            elif self._bridge_host == 'STRIP'
+                #upstream volttron instance (SmartHub) - register with smarthub VolttronBridge
+                url_root = 'http://' + self._up_ip_addr + ':' + self._up_port + '/VolttronBridge'
+                result = self.do_rpc(url_root, 'rpc_registerDsBridge', 
+                                    {'discovery_address': self._discovery_address, 
+                                        'deviceId': self._deviceId
+                                    })
+                if result == 'SUCCESS':
+                    self._usConnected = True
+                
+            return
+            
         @Core.receiver('onstop')
         def onstop(self, sender, **kwargs):
             _log.debug('onstop()')
             return
 
-        @PubSub.subscribe('pubsub', topic_price_point)
-        def on_match_currentPP(self, peer, sender, bus,
-                topic, headers, message):
-            _log.debug('on_match_currentPP()')
-            self.uiPostCurrentPricePoint(headers, message)
+        @RPC.export
+        def rpc_from_net(self, header, message):
+            result = False
+            try:
+                rpcdata = jsonrpc.JsonRpcData.parse(message)
+                _log.info('rpc method: {}'.format(rpcdata.method) + \
+                            '; rpc params: {}'.format(rpcdata.params))
+                
+                if rpcdata.method == "rpc_registerDsBridge":
+                    args = {'discovery_address': rpcdata.params['discovery_address'],
+                            'deviceId':rpcdata.params['deviceId']
+                            }
+                    result = self._registerDsBridge(**args)
+                elif rpcdata.method == "rpc_postEnergyDemand":
+                    args = {'discovery_address': rpcdata.params['discovery_address'],
+                            'deviceId':rpcdata.params['deviceId'],
+                            'newEnergyDemand': rpcdata.params['newEnergyDemand']
+                            }
+                    result = self._postEnergyDemand(**args)    
+                return jsonrpc.json_result(rpcdata.id, result)
+            except AssertionError:
+                print('AssertionError')
+                return jsonrpc.json_error('NA', INVALID_REQUEST,
+                        'Invalid rpc data {}'.format(data))
+            except Exception as e:
+                print(e)
+                return jsonrpc.json_error('NA', UNHANDLED_EXCEPTION, e)
             
-        @PubSub.subscribe('pubsub', topic_sensorsLevelAll_point)
-        def on_match_sensorData(self, peer, sender, bus,
-                topic, headers, message):
-            _log.debug('on_match_sensorData()')
-            self.uiPostSensorData(headers, message)
+        def _registerDsBridge(self, discovery_address, deviceId):
+            if discovery_address in ds_voltBr:
+                _log.debug('already registered')
+                return True
+                
+            ds_voltBr.append(discovery_address)
+            index = ds_voltBr.index(discovery_address)
+            ds_deviceId.insert(index, deviceId)
+            return True
             
-        @PubSub.subscribe('pubsub', topic_ledState_point)
-        def on_match_ledState(self, peer, sender, bus,
-                topic, headers, message):
-            _log.debug('on_match_ledState()')
-            self.uiPostLedState(headers, message)
+        def _postEnergyDemand(self, discovery_address, deviceId, newEnergyDemand):
+            if discovery_address in ds_voltBr:
+                index = ds_voltBr.index(discovery_address)
+                if ds_deviceId(index) == deviceId:
+                    #post to bus
+                    pubTopic = energyDemand_topic + "/" + deviceId
+                    pubMsg = [newEnergyDemand,{'units': 'mW', 'tz': 'UTC', 'type': 'float'}]
+                    self._publishToBus(pubTopic, pubMsg)
+                    return True
+            return False
             
-        @PubSub.subscribe('pubsub', topic_fanState_point)
-        def on_match_fanState(self, peer, sender, bus,
-                topic, headers, message):
-            _log.debug('on_match_fanState()')
-            self.uiPostFanState(headers, message)
-            
-        @PubSub.subscribe('pubsub', topic_ledLevel_point)
-        def on_match_ledLevel(self, peer, sender, bus,
-                topic, headers, message):
-            _log.debug('on_match_ledLevel()')
-            self.uiPostLedLevel(headers, message)
-            
-        @PubSub.subscribe('pubsub', topic_fanLevel_point)
-        def on_match_fanLevel(self, peer, sender, bus,
-                topic, headers, message):
-            _log.debug('on_match_fanLevel()')
-            self.uiPostFanLevel(headers, message)
-            
-        @PubSub.subscribe('pubsub', topic_ledThPP_point)
-        def on_match_ledThPP(self, peer, sender, bus,
-                topic, headers, message):
-            _log.debug('on_match_ledThPP()')
-            self.uiPostLedThPP(headers, message)
-            
-        @PubSub.subscribe('pubsub', topic_fanThPP_point)
-        def on_match_fanThPP(self, peer, sender, bus,
-                topic, headers, message):
-            _log.debug('on_match_fanThPP()')
-            self.uiPostFanThPP(headers, message)
-
-        def uiPostCurrentPricePoint(self, headers, message):
-            #json rpc to BLESmartHubSrv
-            _log.debug('uiPostCurrentPricePoint()')
-            pricePoint = message[0]
-            self.do_rpc('currentPricePoint', {'pricePoint': pricePoint})
-
-        def uiPostSensorData(self, headers, message) :
-            _log.debug('uiPostSensorData()')
-            luxLevel   = message[0]['luxlevel']
-            rhLevel    = message[0]['rhlevel']
-            tempLevel  = message[0]['templevel']
-            co2Level   = message[0]['co2level']
-            pirLevel   = message[0]['pirlevel']
-            self.do_rpc('shSensorsData', {\
-                                            'luxLevel': luxLevel, \
-                                            'rhLevel':  rhLevel, \
-                                            'tempLevel': tempLevel, \
-                                            'co2Level': co2Level, \
-                                            'pirLevel': pirLevel \
-                                            })
-            return
-            
-        def uiPostLedState(self, headers, message) :
-            _log.debug('uiPostLedState()')
-            state = message[0]
-            self.do_rpc('shDeviceState', {'deviceId': SH_DEVICE_LED,
-                                            'state': state})
-            return
-        def uiPostFanState(self, headers, message) :
-            _log.debug('uiPostFanState()')
-            state = message[0]
-            self.do_rpc('shDeviceState', {'deviceId': SH_DEVICE_FAN,
-                                            'state': state})
-            return
-        def uiPostLedLevel(self, headers, message) :
-            _log.debug('uiPostLedLevel()')
-            level = message[0]
-            self.do_rpc('shDeviceLevel', {'deviceId': SH_DEVICE_LED,
-                                            'level': level})
-            return
-        def uiPostFanLevel(self, headers, message) :
-            _log.debug('uiPostFanLevel()')
-            level = message[0]
-            self.do_rpc('shDeviceLevel', {'deviceId': SH_DEVICE_FAN,
-                                            'level': level})
-            return
-        def uiPostLedThPP(self, headers, message) :
-            _log.debug('uiPostLedThPP()')
-            thPP = message[0]
-            self.do_rpc('shDeviceThPP', {'deviceId': SH_DEVICE_LED,
-                                            'thPP': thPP})
-            return
-        def uiPostFanThPP(self, headers, message) :
-            _log.debug('uiPostFanThPP()')
-            thPP = message[0]
-            self.do_rpc('shDeviceThPP', {'deviceId': SH_DEVICE_FAN,
-                                            'thPP': thPP})
-            return
-            
-        def do_rpc(self, method, params=None ):
+        def _publishToBus(self, pubTopic, pubMsg):
+            now = datetime.datetime.utcnow().isoformat(' ') + 'Z'
+            headers = {headers_mod.DATE: now}          
+            #Publish messages
+            self.vip.pubsub.publish('pubsub', pubTopic, headers, pubMsg).get(timeout=5)
+        
+        return
+        
+        def do_rpc(self, url_root, method, params=None ):
+            result = 'FAILED'
             json_package = {
                 'jsonrpc': '2.0',
                 'id': self._agent_id,
@@ -225,12 +244,13 @@ def volttronbridge(config_path, **kwargs):
 
             data = json.dumps(json_package)
             try:
-                response = requests.post(self.url_root, data=json.dumps(json_package))
+                response = requests.post(url_root, data=json.dumps(json_package))
                 
                 if response.ok:
                     success = response.json()['result']
                     if success:
                         _log.debug('response - ok, {} result:{}'.format(method, success))
+                        result = 'SUCCESS'
                     else:
                         _log.debug('respone - not ok, {} result:{}'.format(method, success))
                 else :
