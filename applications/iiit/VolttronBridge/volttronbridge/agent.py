664c4d758d3985a4e29f9445fc1a9eaf5f0c6709
sambabug <sambabug@gmail.com>
2019-10-01 11:36:24 +0530

    Update agent.py

--- applications/iiit/VolttronBridge/volttronbridge/agent.py
+++ applications/iiit/VolttronBridge/volttronbridge/agent.py
@@ -318,7 +318,7 @@ def volttronbridge(config_path, **kwargs):
             _log.debug('_usConnected: ' + str(self._usConnected))
 
             #we want to post to us only if there is change in energy demand
-            if self._isclose(self._ed_current, newEnergyDemand, EPSILON) and re_post == False:
+            if self._isclose(self._ed_current, self._ed_previous, EPSILON) and re_post == False:
                 _log.debug('No change in energy demand, do nothing')
                 return
 
