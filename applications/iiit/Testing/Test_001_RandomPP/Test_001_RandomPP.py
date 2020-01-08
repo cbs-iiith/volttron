15fb29a90c828dd36b1880eb9dc52a24944f6b7d
sambabug <sambabug@gmail.com>
2020-01-08 23:47:18 +0530

    updated testscript default values

--- applications/iiit/Testing/Test_001_RandomPP/Test_001_RandomPP.py
+++ applications/iiit/Testing/Test_001_RandomPP/Test_001_RandomPP.py
@@ -11,6 +11,7 @@
 
 #Sam
 
+from os.path import basename
 import requests
 import sys
 import json
@@ -18,13 +19,17 @@ import math, random
 import threading, time, signal
 from datetime import timedelta
 
+from test_utils import get_timestamp
+
 authentication=None
 
 #WAIT_TIME_SECONDS = 10             # 10 sec
-WAIT_TIME_SECONDS = 5 * 60         # 5 min
+#WAIT_TIME_SECONDS = 5 * 60         # 5 min
 #WAIT_TIME_SECONDS = 15 * 60        # 15 min
 #WAIT_TIME_SECONDS = 30 * 60        # 30 min
-#WAIT_TIME_SECONDS = 1 * 60 * 60    # 1 hour
+WAIT_TIME_SECONDS = 1 * 60 * 60    # 1 hour
+
+ROOT_URL = 'http://192.168.1.11:8080/PricePoint'
 
 class ProgramKilled(Exception):
     pass
@@ -34,7 +39,7 @@ def signal_handler(signum, frame):
     
 def do_rpc(method, params=None ):
     global authentication
-    url_root = 'http://192.168.1.11:8080/PricePoint'
+    url_root = ROOT_URL
     
     json_package = {
         'jsonrpc': '2.0',
@@ -57,24 +62,25 @@ def post_random_price():
     no_digit = 2
     pp = math.floor(random.random()*10**no_digit)/10**no_digit
     pp = .94 if pp>.94 else pp
-    print "Time: " + str(time.ctime()) + "; PricePoint: " + str(pp) +";",
+    print get_timestamp() + ' PricePoint: ' + str(pp) + ',',
     try:
         response = do_rpc("rpc_updatePricePoint", {'newPricePoint': pp})
         #print "response: " +str(response),
         if response.ok:
             success = response.json()['result']
             if success:
-                print "new price updated"
+                print 'new price updated'
             else:
-                print "new price NOT updated"
+                print 'new price NOT updated'
         else:
-            print "do_rpc pricepoint response NOT OK"
+            print 'do_rpc pricepoint response NOT OK'
     except KeyError:
         error = response.json()['error']
         print (error)
     except Exception as e:
         #print (e)
-        print "do_rpc() unhandled exception, most likely server is down"
+        print 'do_rpc() unhandled exception, most likely server is down'
+    sys.stdout.flush()
     return
     
 class Job(threading.Thread):
@@ -96,16 +102,21 @@ class Job(threading.Thread):
             self.execute(*self.args, **self.kwargs)
                 
 if __name__ == '__main__':
+    print get_timestamp() + ' Initialising test - ' + basename(__file__) + ' ...'
     signal.signal(signal.SIGTERM, signal_handler)
     signal.signal(signal.SIGINT, signal_handler)
     job = Job(interval=timedelta(seconds=WAIT_TIME_SECONDS), execute=post_random_price)
+    print get_timestamp() + ' ROOT_URL: ' + str(ROOT_URL) + ', WAIT_TIME_SECONDS: ' + str(WAIT_TIME_SECONDS)
+    print get_timestamp() + ' Starting a repetitive job...'
+    sys.stdout.flush()
     job.start()
     
     while True:
           try:
               time.sleep(1)
           except ProgramKilled:
-              print "\nProgram killed: running cleanup code"
+              print '\n' + get_timestamp() + ' Program killed: running cleanup code'
               job.stop()
+              print get_timestamp() + ' End of Program.'
               break
               
