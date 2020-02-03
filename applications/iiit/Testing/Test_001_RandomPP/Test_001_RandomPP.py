fb71eb10235468d01c7659b07ad9f7eb5be3bf5a
sambabug <sambabug@gmail.com>
2020-02-03 22:56:02 +0530

    ...

--- applications/iiit/Testing/Test_001_RandomPP/Test_001_RandomPP.py
+++ applications/iiit/Testing/Test_001_RandomPP/Test_001_RandomPP.py
@@ -24,6 +24,7 @@ from test_utils import get_timestamp
 
 authentication=None
 
+#WAIT_TIME_SECONDS = 2             # 2 sec
 #WAIT_TIME_SECONDS = 10             # 10 sec
 #WAIT_TIME_SECONDS = 5 * 60         # 5 min
 #WAIT_TIME_SECONDS = 15 * 60        # 15 min
@@ -63,41 +64,41 @@ def post_random_price():
     no_digit = 2
     pp = math.floor(random()*10**no_digit)/10**no_digit
     pp = .94 if pp>.94 else pp
-    print (get_timestamp() + ' PricePoint: ' + str(pp) + ',',)
+    print get_timestamp() + ' PricePoint: ' + str(pp) + ',',
     now = datetime.datetime.utcnow().isoformat(' ') + 'Z'
     try:
-        response = do_rpc('new-pp', {'msg_type' = 0
-                                    , 'one_to_one' = False
-                                    , 'value' = pp
-                                    , 'value_data_type' = 'float'
-                                    , 'units' = 'cents'
-                                    , 'price_id' = randint(0, 99999999)
-                                    , 'isoptimal' = True
-                                    , 'src_ip' = None
-                                    , 'src_device_id' = None
-                                    , 'dst_ip' = None
-                                    , 'dst_device_id' = None
-                                    , 'duration' = WAIT_TIME_SECONDS
-                                    , 'ttl' = 10
-                                    , 'ts' = now
-                                    ,'tz' = 'UTC'
+        response = do_rpc('new-pp', {'msg_type': 0
+                                    , 'one_to_one': False
+                                    , 'value': pp
+                                    , 'value_data_type': 'float'
+                                    , 'units': 'cents'
+                                    , 'price_id': randint(0, 99999999)
+                                    , 'isoptimal': True
+                                    , 'src_ip': None
+                                    , 'src_device_id': None
+                                    , 'dst_ip': None
+                                    , 'dst_device_id': None
+                                    , 'duration': WAIT_TIME_SECONDS
+                                    , 'ttl': 10
+                                    , 'ts': now
+                                    ,'tz': 'UTC'
                                     })
         #print "response: " +str(response),
         if response.ok:
             if 'result' in response.json().keys():
                 if response.json()['result']:
-                    print ('new price updated!!!')
+                    print 'new price updated!!!'
                 else:
-                    print ('new price NOT updated!!!')
+                    print 'new price NOT updated!!!'
             elif 'error' in response.json().keys():
-                print (response.json()['error'])
+                print response.json()['error']
         else:
-            print ('do_rpc pricepoint response NOT OK, response: {}'.format(response))
+            print 'do_rpc pricepoint response NOT OK, response: {}'.format(response)
     except KeyError as ke:
-        print (ke)
+        print ke
     except Exception as e:
-        print (e)
-        print ('do_rpc() unhandled exception, most likely server is down')
+        print e
+        print 'do_rpc() unhandled exception, most likely server is down'
     sys.stdout.flush()
     return
     
@@ -120,12 +121,12 @@ class Job(threading.Thread):
             self.execute(*self.args, **self.kwargs)
                 
 if __name__ == '__main__':
-    print (get_timestamp() + ' Initialising test - ' + basename(__file__) + ' ...')
+    print get_timestamp() + ' Initialising test - ' + basename(__file__) + ' ...'
     signal.signal(signal.SIGTERM, signal_handler)
     signal.signal(signal.SIGINT, signal_handler)
     job = Job(interval = datetime.timedelta(seconds=WAIT_TIME_SECONDS), execute=post_random_price)
-    print (get_timestamp() + ' ROOT_URL: ' + str(ROOT_URL) + ', WAIT_TIME_SECONDS: ' + str(WAIT_TIME_SECONDS))
-    print (get_timestamp() + ' Starting a repetitive job...')
+    print get_timestamp() + ' ROOT_URL: ' + str(ROOT_URL) + ', WAIT_TIME_SECONDS: ' + str(WAIT_TIME_SECONDS)
+    print get_timestamp() + ' Starting a repetitive job...'
     sys.stdout.flush()
     job.start()
     
@@ -133,8 +134,8 @@ if __name__ == '__main__':
           try:
               time.sleep(1)
           except ProgramKilled:
-              print ('\n' + get_timestamp() + ' Program killed: running cleanup code')
+              print '\n' + get_timestamp() + ' Program killed: running cleanup code'
               job.stop()
-              print (get_timestamp() + ' End of Program.')
+              print get_timestamp() + ' End of Program.'
               break
               
