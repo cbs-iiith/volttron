c7c28d048bb3e551ac5d232e2a657ea2a0746bdd
sambabug <sambabug@gmail.com>
2020-02-04 15:14:40 +0530

    Update test2.py

--- applications/iiit/Utils/test2.py
+++ applications/iiit/Utils/test2.py
@@ -17,9 +17,19 @@ from ispace_msg import ISPACE_Msg, MessageType
 from ispace_msg_utils import get_default_pp_msg
 from copy import copy
 
-
 tmp1 = get_default_pp_msg('192.168.1.4', 'device-1')
-print tmp1
-tmp1.set_value(20)
-tmp1 = tmp1 + 10
-print tmp1
+tmp2 = get_default_pp_msg('192.168.1.4', 'device-2')
+tmp3 = get_default_pp_msg('192.168.1.4', 'device-3')
+print 'step {}: {}'.format(1, tmp1)
+print 'step {}: {}'.format(2, tmp2)
+print 'step {}: {}'.format(3, tmp3)
+tmp1.set_value(.10)
+tmp2.set_value(.20)
+tmp3.set_value(.30)
+print 'step {}: {}'.format(4, tmp1)
+tmp1 = tmp1 + .10
+print 'step {}: {}'.format(5, tmp1)
+#tmp2 = 0.20 + tmp2
+#print 'step {}: {}'.format(6, tmp2)
+tmp3 += 0.3
+print 'step {}: {}'.format(7, tmp3)
