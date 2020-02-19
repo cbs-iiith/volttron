5314a2431d743de107e6a180d1669b8999a6ecbe
sambabug <sambabug@gmail.com>
2020-02-19 16:12:09 +0530

    Update test2.py

--- applications/iiit/Utils/test2.py
+++ applications/iiit/Utils/test2.py
@@ -20,16 +20,30 @@ from copy import copy
 tmp1 = get_default_pp_msg('192.168.1.4', 'device-1')
 tmp2 = get_default_pp_msg('192.168.1.4', 'device-2')
 tmp3 = get_default_pp_msg('192.168.1.4', 'device-3')
-print 'step {}: {}'.format(1, tmp1)
-print 'step {}: {}'.format(2, tmp2)
-print 'step {}: {}'.format(3, tmp3)
-tmp1.set_value(.10)
-tmp2.set_value(.20)
-tmp3.set_value(.30)
-print 'step {}: {}'.format(4, tmp1)
-tmp1 = tmp1 + .10
-print 'step {}: {}'.format(5, tmp1)
-#tmp2 = 0.20 + tmp2
-#print 'step {}: {}'.format(6, tmp2)
-tmp3 += 0.3
-print 'step {}: {}'.format(7, tmp3)
+tmp4 = get_default_pp_msg('192.168.1.4', 'device-4')
+tmp5 = get_default_pp_msg('192.168.1.4', 'device-5')
+tmp6 = get_default_pp_msg('192.168.1.4', 'device-6')
+
+messages_list = [tmp1, tmp2, tmp3, tmp4, tmp5, tmp6]
+del_count = 0
+
+messages = copy(messages_list)
+print('messages:')
+msg_count = len(messages_list)
+for idx, pp_msg in enumerate(messages_list):
+    print('           msg_no: {} of {}, message: {}'.format(idx+1, msg_count, pp_msg))
+    
+msg_count = len(messages)
+for idx, pp_msg in enumerate(messages):
+    print('processing msg {:d}/{:d}'.format(idx+1, msg_count))
+    if (pp_msg.get_src_device_id() == 'device-2' 
+        or pp_msg.get_src_device_id() == 'device-5'
+        ):
+        del messages_list[idx - del_count]
+        del_count += 1
+    continue
+
+print('messages:')
+msg_count = len(messages_list)
+for idx, pp_msg in enumerate(messages_list):
+    print('           msg_no: {} of {}, message: {}'.format(idx+1, msg_count, pp_msg))
