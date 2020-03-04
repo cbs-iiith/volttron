339eecada03203376ef31f7b46cf430e1bbbc87d
Sam Babu G <sambabug@gmail.com>
2020-03-04 12:36:43 +0530

    ...

--- applications/iiit/Utils/ispace_utils.py
+++ applications/iiit/Utils/ispace_utils.py
@@ -15,24 +15,25 @@ import datetime
 import logging
 import math
 import time
+
 import gevent
 import gevent.event
 from gevent import monkey
+
 monkey.patch_all(thread=False, select=False)
 # if using grequests, comment the above two (i.e., monkey.patch_all())
-#import grequests
+# import grequests
 import requests
 
-from enum import IntEnum
 import json
 
 from volttron.platform.agent import utils
 from volttron.platform.messaging import headers as headers_mod
 from volttron.platform.agent.known_identities import MASTER_WEB
-from volttron.platform.agent import json as jsonapi
 
 utils.setup_logging()
 _log = logging.getLogger(__name__)
+
 
 # some agent needs to communicate with the local bridge
 # for example: pca --> to get the list of local/ds devices
@@ -52,10 +53,11 @@ def ping_vb_failed(self):
     except Exception as e:
         # print(e)
         _log.warning('Bridge Agent is not responding!!!'
-            + ' Message: {}'.format(e.message)
-        )
+                     + ' Message: {}'.format(e.message)
+                     )
         pass
     return True
+
 
 # register agent with local vb, blocking fucntion
 # register this agent with vb as local device for posting active power & bid
@@ -87,6 +89,7 @@ def register_with_bridge(self, sleep_time=10):
         time.sleep(sleep_time)
     return
 
+
 def unregister_with_bridge(self):
     try:
         _log.info('Unregistering with the bridge...')
@@ -106,6 +109,7 @@ def unregister_with_bridge(self):
         pass
     return
 
+
 # register rpc routes with MASTER_WEB
 def register_rpc_route(self, name, handle, sleep_time=10):
     while True:
@@ -124,18 +128,19 @@ def register_rpc_route(self, name, handle, sleep_time=10):
                 break
         except gevent.Timeout:
             _log.warning('maybe the Volttron instance is not yet ready!!!'
-                + ' message: {}'.format(e.message)
-            )
+                         + ' message: {}'.format(e.message)
+                         )
             pass
         except Exception as e:
             # print(e)
             _log.warning('maybe the Volttron instance is not yet ready!!!'
-                + ' message: {}'.format(e.message)
-            )
+                         + ' message: {}'.format(e.message)
+                         )
             pass
         _log.warning('will try again in {} sec'.format(sleep_time))
         time.sleep(sleep_time)
     return
+
 
 # try to retrive self._device_id, self._ip_addr, self._discovery_address
 # from bridge agent
@@ -159,20 +164,21 @@ def retrive_details_from_vb(self, sleep_time=10):
                     'discovery_address'
                 ).get(timeout=10)
                 self._discovery_address = discovery_address
-                _log.debug('discovery_address as per vb'
-                    + ' : {}'.format(discovery_address)
+                _log.debug('discovery_address as per vb{0}'.format(
+                    ' : {}'.format(discovery_address))
                 )
             if (device_id is not None and discovery_address is not None):
                 _log.info('done.')
                 break
         except Exception as e:
-            _log.warning ('unable to retrive details from bridge'
-                + ', message: {}.'.format(e.message)
-            )
+            _log.warning('unable to retrive details from bridge'
+                         + ', message: {}.'.format(e.message)
+                         )
             pass
         _log.warning('will try again in {} sec'.format(sleep_time))
         time.sleep(sleep_time)
     return
+
 
 def publish_to_bus(self, topic, msg):
     # _log.debug('publish_to_bus()')
@@ -195,6 +201,7 @@ def publish_to_bus(self, topic, msg):
         return
     return
 
+
 def get_task_schdl(self, task_id, device, time_ms=None):
     # _log.debug('get_task_schdl()')
     self.time_ms = 600 if time_ms is None else time_ms
@@ -203,7 +210,7 @@ def get_task_schdl(self, task_id, device, time_ms=None):
         end = str(
             datetime.datetime.now()
             + datetime.timedelta(milliseconds=self.time_ms)
-            )
+        )
 
         msg = [[device, start, end]]
         result = self.vip.rpc.call(
@@ -221,11 +228,12 @@ def get_task_schdl(self, task_id, device, time_ms=None):
         _log.warning('gevent.Timeout for task_id: {}'.format(task_id))
         return False
     except Exception as e:
-        _log.warning ('Could not contact actuator for task_id: {}.'
-            + ' Is it running? {}'.format(task_id, e.message))
+        _log.warning('Could not contact actuator for task_id: {}.'
+                     + ' Is it running? {}'.format(task_id, e.message))
         return False
         # print(e)
     return True
+
 
 def cancel_task_schdl(self, task_id):
     # _log.debug('cancel_task_schdl()')
@@ -240,11 +248,13 @@ def cancel_task_schdl(self, task_id):
         _log.warning(
             'cancel_task_schdl() for task_id ({}) failed.'.format(task_id)
             + ' message: {}'.format(e.message))
-    return
+    return result
+
 
 def mround(num, multipleOf):
     # _log.debug('mround()')
     return (math.floor((num + multipleOf / 2) / multipleOf) * multipleOf)
+
 
 # refer to https://bit.ly/3beuacI
 # What is the best way to compare floats for almost-equality in Python?
@@ -253,13 +263,16 @@ def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
     # _log.debug('isclose()')
     return (abs(a - b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol))
 
+
 # return energy in kWh for the given duration
 def calc_energy_kwh(pwr_wh, duration_sec):
     return ((pwr_wh * duration_sec) / 3600000)
 
+
 # return energy in Wh for the given duration
 def calc_energy_wh(pwr_wh, duration_sec):
     return ((pwr_wh * duration_sec) / 3600)
+
 
 # TODO: do_rpc is synchrous, using requests which is a blocking operation
 # need to convert to async operations, maybe can use gevent
@@ -298,9 +311,9 @@ def do_rpc(id, url_root, method, params=None, request_method='POST'):
         else:
             if request_method.upper() != 'GET':
                 _log.warning('unimplemented'
-                    + ' request_method: {}'.format(request_method)
-                    + ', trying "GET"!!!'
-                )
+                             + ' request_method: {}'.format(request_method)
+                             + ', trying "GET"!!!'
+                             )
             response = requests.get(
                 url_root,
                 data=json.dumps(json_package),
@@ -319,68 +332,71 @@ def do_rpc(id, url_root, method, params=None, request_method='POST'):
                     result = True
                 else:
                     _log.debug('respone - ok'
-                        + ', {} result success: {}'.format(method, success)
-                    )
+                               + ', {} result success: {}'.format(method,
+                                                                  success)
+                               )
             elif 'error' in response.json().keys():
                 error = response.json()['error']
                 _log.warning('{} returned error'.format(method)
-                    + ', Error: {}'.format(error)
-                )
+                             + ', Error: {}'.format(error)
+                             )
         else:
             _log.warning('no respone, url_root:{}'.format(url_root)
-                + ' method:{}'.format(method)
-                + ' response: {}'.format(response)
-            )
+                         + ' method:{}'.format(method)
+                         + ' response: {}'.format(response)
+                         )
     except requests.exceptions.HTTPError as rhe:
         _log.warning('do_rpc() requests http error occurred.'
-            + ' Check the url'
-            + ', message: {}'.format(rhe.message)
-        )
-        pass
-    except requests.exceptions.ConnectionError as rce:
-        _log.warning('do_rpc() requests connection error.'
-            + ' Most likely dest is down'
-            + ', message: {}'.format(rce.message)
-        )
+                     + ' Check the url'
+                     + ', message: {}'.format(rhe.message)
+                     )
         pass
     except requests.exceptions.ConnectTimeout as rcte:
         _log.warning('do_rpc() requests connect timed out'
-            + ' while trying connect to remote.'
-            + ' Maybe set up for a retry or continue in a retry loop'
-            + ', message: {}'.format(rcte.message)
-        )
+                     + ' while trying connect to remote.'
+                     + ' Maybe set up for a retry or continue in a retry loop'
+                     + ', message: {}'.format(rcte.message)
+                     )
+        pass
+    except requests.exceptions.ConnectionError as rce:
+        _log.warning('do_rpc() requests connection error.'
+                     + ' Most likely dest is down'
+                     + ', message: {}'.format(rce.message)
+                     )
         pass
     except requests.exceptions.ReadTimeout as rrte:
         _log.warning('do_rpc() requests read timed out.'
-            + ' Dst did not send any data in the allotted amount of time'
-            + ', message: {}'.format(rrte.message)
-        )
+                     + ' Dst did not send any data in the allotted amount of '
+                       'time'
+                     + ', message: {}'.format(rrte.message)
+                     )
         pass
     except requests.exceptions.Timeout as rte:
         _log.warning('do_rpc() requests timed out.'
-            + ' Maybe set up for a retry or continue in a retry loop'
-            + ', message: {}'.format(rte.message)
-        )
+                     + ' Maybe set up for a retry or continue in a retry loop'
+                     + ', message: {}'.format(rte.message)
+                     )
         pass
     except requests.exceptions.TooManyRedirects as rre:
         _log.warning('do_rpc() too many redirects exception.'
-            + ' Most likely URL was bad'
-            + ', message: {}'.format(rre.message)
-        )
+                     + ' Most likely URL was bad'
+                     + ', message: {}'.format(rre.message)
+                     )
         pass
     except requests.exceptions.RequestException as re:
         _log.warning('do_rpc() unhandled requests exception.'
-            + ' Bailing out'
-            + ', message: {}'.format(re.message)
-        )
+                     + ' Bailing out'
+                     + ', message: {}'.format(re.message)
+                     )
         pass
     except Exception as e:
         # print(e)
         _log.warning('do_rpc() unhandled exception, most likely dest is down'
-            + ', message: {}'.format(e.message)
-        )
+                     + ', message: {}'.format(e.message)
+                     )
         pass
     return result
+
 
 '''
     # https://bit.ly/37OaR7i
@@ -431,30 +447,31 @@ def do_rpc(id, url_root, method, params=None, request_method='POST'):
 '''
 
 
-class Runningstats():
-    '''
+class Runningstats:
+    """
+    The code is an extension of the method of Knuth and Welford for computing
+    standard deviation in one pass through the data. It computes skewness and
+    kurtosis as well with a similar interface.
+
     "https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Welford's
     _online_algorithm"
     https://www.johndcook.com/blog/standard_deviation/
     https://www.johndcook.com/blog/skewness_kurtosis/
-    The code is an extension of the method of Knuth and Welford for computing
-    standard deviation in one pass through the data. It computes skewness and
-    kurtosis as well with a similar interface.
-    
+
     mean()
     Variance()
     std_dev()
     skewness()
     kurtosis()
-    
+
     "https://stackoverflow.com/questions/12636613/how-to-calculate-moving-
     average-without-keeping-the-count-and-data-total"
-    
-    '''
-    n = 0
+
+    """
+    n = 0  # type: int
     M1 = M2 = M3 = M4 = 0.0
 
-    #Exponential weighted moving average
+    # Exponential weighted moving average
     exp_avg = 0.0
     # factor is a constant that affects how quickly the average "catches up" to
     # the latest trend. Smaller the number the faster. (At 1 it's no longer an
@@ -462,16 +479,17 @@ class Runningstats():
     # assuming 2 reading per minute, 1 hour = 120 reading
     factor = 120
 
-    def __init__(self, factor = 120):
+    def __init__(self, factor=120):
         self.clear()
         self.factor = factor
         pass
 
     ''' overload methods for self and other
     '''
+
     # overload + operator
     def __add__(self, other):
-        combined = Runningstats(self.factor)
+        combined = Runningstats(self.factor)  # type: Runningstats
 
         combined.n = self.n + other.n
 
@@ -481,14 +499,24 @@ class Runningstats():
         delta4 = delta2 * delta2
 
         combined.M1 = (self.n * self.M1 + other.n * other.M1) / combined.n
-        
-        combined.M2 = self.M2 + other.M2 + delta2 * self.n * other.n / combined.n
 
-        combined.M3 = self.M3 + other.M3 + delta3 * self.n * other.n * (self.n - other.n)/(combined.n*combined.n)
-        combined.M3 += 3.0 * delta * (self.n * other.M2 - other.n * self.M2) / combined.n
+        combined.M2 = self.M2 + other.M2 + delta2 * self.n * other.n / \
+                      combined.n
 
-        combined.M4 = self.M4 + other.M4 + delta4 *self.n * other.n * (self.n * self.n - self.n  *other.n + other.n * other.n) / (combined.n *combined.n * combined.n)
-        combined.M4 += 6.0 * delta2 * (self.n*self.n*other.M2 + other.n*other.n*self.M2)/(combined.n * combined.n) + 4.0 * delta * (self.n * other.M3 - other.n * self.M3) / combined.n
+        combined.M3 = self.M3 + other.M3 + delta3 * self.n * other.n * (
+                self.n - other.n) / (combined.n * combined.n)
+        combined.M3 += 3.0 * delta * (
+                self.n * other.M2 - other.n * self.M2) / combined.n
+
+        combined.M4 = self.M4 + other.M4 + delta4 * self.n * other.n * (
+                self.n * self.n - self.n * other.n + other.n * other.n) / (
+                              combined.n * combined.n * combined.n)
+        combined.M4 += 6.0 * delta2 * (
+                self.n * self.n * other.M2 + other.n * other.n * self.M2) \
+                       / (
+                               combined.n * combined.n) + 4.0 * delta * (
+                               self.n * other.M3 - other.n * self.M3) / \
+                       combined.n
         return combined
 
     ''' ENDOF overload methods for self and value
@@ -498,11 +526,11 @@ class Runningstats():
         self.n = 0
         self.M1 = self.M2 = self.M3 = self.M4 = 0.0
 
-    def push(self, x = 0.0):
-        delta = 0.0
-        delta_n = 0.0
-        delta_n2 = 0.0
-        term1 = 0.0
+    def push(self, x=0.0):
+        delta = 0.0  # type: float
+        delta_n = 0.0  # type: float
+        delta_n2 = 0.0  # type: float
+        term1 = 0.0  # type: float
 
         n1 = self.n
 
@@ -513,19 +541,19 @@ class Runningstats():
         delta_n2 = delta_n * delta_n
         term1 = delta * delta_n * n1
 
-        self.M1 += delta_n;
+        self.M1 += delta_n
 
         self.M4 += (
-            (term1 * delta_n2 * ((self.n * self.n) - (3 * self.n) + 3)) +
-            (6 * delta_n2 * self.M2) -
-            (4 * delta_n * self.M3)
-            )
+                (term1 * delta_n2 * ((self.n * self.n) - (3 * self.n) + 3)) +
+                (6 * delta_n2 * self.M2) -
+                (4 * delta_n * self.M3)
+        )
 
         self.M3 += (term1 * delta_n * (self.n - 2)) - (3 * delta_n * self.M2)
 
         self.M2 += term1
 
-        #Exponential weighted moving average
+        # Exponential weighted moving average
         self.exp_avg += (x - self.exp_avg) / min(self.n, self.factor)
 
         return
@@ -537,17 +565,16 @@ class Runningstats():
         return self.M1
 
     def variance(self):
-        return (self.M2/(self.n - 1))
+        return self.M2 / (self.n - 1)
 
     def std_dev(self):
-        return (math.sqrt(self.variance()))
+        return math.sqrt(self.variance())
 
     def skewness(self):
-        return (math.sqrt(self.n) * self.M3 / pow(self.M2, 1.5))
+        return math.sqrt(self.n) * self.M3 / pow(self.M2, 1.5)
 
     def kurtosis(self):
-        return (self.n * self.M4 / (self.M2 * self.M2) - 3.0)
-        
+        return self.n * self.M4 / (self.M2 * self.M2) - 3.0
+
     def exp_wt_mv_avg(self):
         return self.exp_avg
-
