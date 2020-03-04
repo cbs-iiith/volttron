63f41ff5dc4927a1fc3946f2ba10c8f0befd7878
Sam Babu G <sambabug@gmail.com>
2020-03-04 18:26:51 +0530

    ...

--- applications/iiit/PriceController/pricecontroller/agent.py
+++ applications/iiit/PriceController/pricecontroller/agent.py
@@ -21,11 +21,12 @@ import dateutil
 import gevent
 import gevent.event
 
-from applications.iiit.Utils.ispace_msg import ISPACE_Msg, MessageType, EnergyCategory
-from applications.iiit.Utils.ispace_msg_utils import get_default_pp_msg, check_msg_type, ted_helper
-from applications.iiit.Utils.ispace_msg_utils import valid_bustopic_msg, tap_helper
-from applications.iiit.Utils.ispace_utils import publish_to_bus, retrive_details_from_vb
-from applications.iiit.Utils.ispace_utils import register_rpc_route
+from applications.iiit.Utils.ispace_msg import ISPACE_Msg, MessageType, \
+    EnergyCategory
+from applications.iiit.Utils.ispace_msg_utils import get_default_pp_msg, \
+    check_msg_type, ted_helper, valid_bustopic_msg, tap_helper
+from applications.iiit.Utils.ispace_utils import publish_to_bus, \
+    retrive_details_from_vb, register_rpc_route
 from volttron.platform import jsonrpc
 from volttron.platform.agent import utils
 from volttron.platform.agent.known_identities import (
