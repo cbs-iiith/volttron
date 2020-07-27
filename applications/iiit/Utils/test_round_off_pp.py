25649ce6765ffc00e4cdc1cbb4a7a06582fd2b52
Sam Babu G <sambabug@gmail.com>
2020-07-27 12:47:30 +0530

    ...

--- applications/iiit/Utils/test_round_off_pp.py
+++ applications/iiit/Utils/test_round_off_pp.py
@@ -6,7 +6,7 @@ from applications.iiit.Utils.ispace_msg import round_off_pp
 class MyTestCase(unittest.TestCase):
 
     def test_something(self):
-
+        print('round_off_pp(-0.10): {:0.2f}'.format(round_off_pp(-0.10)))
         print('round_off_pp(0): {:0.2f}'.format(round_off_pp(0)))
         print('round_off_pp(.10): {:0.2f}'.format(round_off_pp(.10)))
         print('round_off_pp(1): {:0.2f}'.format(round_off_pp(1)))
